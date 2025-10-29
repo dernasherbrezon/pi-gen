e91771cc8b15adc5208cc8c3848ea33381b793e9
dernasherbrezon <dernasherbrezon@gmail.com>
2025-10-29 17:35:46 +0000

    fix the build

--- stage0/00-configure-apt/00-run.sh
+++ stage0/00-configure-apt/00-run.sh
@@ -6,7 +6,7 @@ install -m 644 files/raspi.sources "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 install -m 644 files/r2cloud.sources "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/raspbian.sources"
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/raspi.sources"
-sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/r2cloud.list"
+sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/r2cloud.sources"
 
 if [ -n "$APT_PROXY" ]; then
 	install -m 644 files/51cache "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
